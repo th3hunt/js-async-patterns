f59f129847a13c2d40ec92a574d9dda30b4551f8
Stratos Pavlakis <pavlakis.stratos@gmail.com>
2016-01-24 21:44:29 +0200

    Add lineage example for callbacks

---
+++ callbacks/lineage.js
@@ -0,0 +1,52 @@
+import fakeAjax from '../fakeAjax.js';
+
+function onError(status, text) {
+  console.log('Error: %s', text);
+  console.log('Falling back to %s', 'lotr.com/arvedui');
+  fakeAjax('lotr.com/arvedui', character => console.log('> %s', character.title));
+}
+
+fakeAjax('lotr.com/aragorn',
+  (character) => {
+    console.log('> %s\n', character.title);
+    fakeAjax(character.ancestor,
+       (character) => {
+         console.log('> %s', character.title);
+         fakeAjax(character.ancestor,
+            (character) => {
+              console.log('> %s', character.title);
+              fakeAjax(character.ancestor,
+                 (character) => {
+                   console.log('> %s', character.title);
+                 },
+                 onError
+               )
+            },
+            onError
+          )
+       },
+       onError
+     )
+  },
+  onError
+);
+
+// OR...
+
+function lineage(url) {
+  fakeAjax(url,
+    character => {
+      console.log('> %s', character.title);
+      if (character.ancestor) {
+        lineage(character.ancestor);
+      }
+    },
+    error => {
+      console.log('Error: %s', error.message);
+      console.log('Falling back to %s', 'lotr.com/arvedui');
+      return lineage('lotr.com/arvedui');
+    }
+  )
+}
+
+// lineage('lotr.com/aragorn');
