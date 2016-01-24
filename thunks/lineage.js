da99f344785067428aa668a33f22494b23f8d69e
Stratos Pavlakis <pavlakis.stratos@gmail.com>
2016-01-24 21:01:40 +0200

    Add a recursive solution to thunks example

--- thunks/lineage.js
+++ thunks/lineage.js
@@ -24,15 +24,49 @@ function showCharacter(error, character) {
     return fetchCharacter(character.ancestor);
   }
 }
+//
+// fetchCharacter('lotr.com/aragorn')(
+//   showCharacter
+// )(
+//   showCharacter
+// )(
+//   showCharacter
+// )(
+//   showCharacter
+// )(
+//   showCharacter
+// );
 
-fetchCharacter('lotr.com/aragorn')(
-  showCharacter
-)(
-  showCharacter
-)(
-  showCharacter
-)(
-  showCharacter
-)(
-  showCharacter
-);
+// OR
+
+function lineage(url) {
+  return fetchCharacter(url)((error, character) => {
+    if (error) {
+      console.log('Error: %s', error.message);
+      console.log('Falling back to %s', 'lotr.com/arvedui');
+      return lineage('lotr.com/arvedui');
+    }
+
+    console.log('> %s', character.title);
+
+    if (character.ancestor) {
+      return lineage(character.ancestor);
+    }
+  });
+}
+
+lineage('lotr.com/aragorn');
+
+// Current seq does not pass results from one to another
+
+// thunk.seq([
+//   fetchCharacter('lotr.com/aragorn'),
+//   fetchCharacter('lotr.com/arathorn'),
+//   fetchCharacter('lotr.com/arador')
+// ])(function (error, characters) {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log(...characters.map(ch => ch.title));
+//   }
+// });
