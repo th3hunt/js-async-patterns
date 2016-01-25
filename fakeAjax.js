a55d67e44a1ffcb8ac27aa9a31bdcc761fff8b75
Stratos Pavlakis <pavlakis.stratos@gmail.com>
2016-01-25 15:18:50 +0200

    Add fake ajax responses for SouthPark scenario

--- fakeAjax.js
+++ fakeAjax.js
@@ -3,6 +3,11 @@ const fakeResponses = {
   'lotr.com/arathorn': {title: 'Arathorn II, Chieftain of the Dúnedain', ancestor: 'lotr.com/arador'},
   'lotr.com/arador': {title: 'Arador, 14th Chieftain of the Dúnedain', ancestor: 'lotr.com/<broken_link>'},
   'lotr.com/arvedui': {title: 'The last King of Arnor'}
+
+  'eric/profile': {id: 1, name: 'Eric Cartman', friends: 3, pictures: 2, episodes: 3},
+  'eric/friends': ['Stan', 'Kyle', 'Kenny'],
+  'eric/pictures': ['avatar.jpg', '4th_grade.jpg'],
+  'eric/episodes': ['Scott Tenorman Must Die', 'Cow Days', 'A Ladder to Heaven']
 };
 
 export default function fakeAjax(url, success, error) {
